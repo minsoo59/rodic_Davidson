637fb3c77741fa03d5c237b2cb039ff9469a0808
shushu <minsoo59@naver.com>
2021-07-30 15:10:55 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,106 @@
-import "./scss/components/App.scss";
-
 function App() {
-  return <h1> hello !</h1>;
+  return (
+    <div className="wrapper">
+      <header>
+        <div className="header__text">
+          <div>Rodić Davidson </div>
+          <div>Architects</div>
+        </div>
+      </header>
+
+      <main>
+        <nav>
+          <ul className="nav__text">
+            <li>
+              <a href="http://localhost:3000">Projects</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Practice</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Expertise</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">News</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Exhibition</a>
+            </li>
+            <li>
+              <a href="http://localhost:3000">Contact</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="main__photos">
+          <div>
+            <img src="https://source.unsplash.com/random/1"></img>
+            <div className="main__text">Instagram</div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/2"></img>
+            <div className="main__text">Five Apartments. Kensington</div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/3"></img>
+            <dv className="main__photo"></dv>
+            <div className="main__text">
+              Queen Anne Apartments, Knightsbridge
+            </div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/4"></img>
+            <div className="main__text">Former Embassy. Kensington</div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/5"></img>
+            <div className="main__text">Town House, Kensington</div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/6"></img>
+            <div className="main__text">New Build Home, St John's Wood</div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/7"></img>
+            <div className="main__text">
+              Planning and listed building consents granted for demolition and
+              new build house in the Royal Borough of Kensington and Chelsea
+            </div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/8"></img>
+            <div className="main__text">Sartor House, Chelsea</div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/9"></img>
+            <div className="main__text">RDA Stairs</div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/10"></img>
+            <div className="main__text">
+              By the Light of a Silbery Moon, J Spenver
+            </div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/11"></img>
+            <div className="main__text">North Vat, Dungeness</div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/12"></img>
+            <div className="main__text">Market Square, Reading</div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/13"></img>
+            <div className="main__text">Five Apartments Kensington</div>
+          </div>
+          <div>
+            <img src="https://source.unsplash.com/random/14"></img>
+            <div className="main__text">Mixed Use, Southwark</div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
